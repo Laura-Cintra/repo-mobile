8cd7f760706749fed67494a0d7d9c1e4f3fcf564
Laura Cintra <79454879+Laura-Cintra@users.noreply.github.com>
2025-08-19 09:47:04 -0300

    feat: firestore item

---
+++ aula03-FireStoreDatabase/app/HomeScreen.tsx
@@ -0,0 +1,92 @@
+import { Text,Button,Alert, TextInput, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useRouter } from "expo-router";
+
+import {auth} from '../services/firebaseConfig'
+import { deleteUser } from "firebase/auth";
+import ItemLoja from "./components/ItemLoja";
+import { useState } from "react";
+import { collection, addDoc, db } from "../services/firebaseConfig";
+
+export default function HomeScreen() {
+    const [ title, setTitle ] = useState('');
+    const router = useRouter()//Hook de navegação entre telas
+    const realizarLogoff = async()=>{
+        await AsyncStorage.removeItem('@user')
+        router.push('/')
+    }
+    const excluirConta = () =>{
+        Alert.alert(
+            "Confirmar Exclusão",
+            "Tem certeza que deseja excluir sua conta? Esta ação não poderá ser desfeita!",
+            [
+                {text:"Cancelar",style:"cancel"},
+                {text:"Excluir",style:'destructive',
+                    onPress:async()=>{
+                        try{
+                            const user = auth.currentUser
+                            if(user){
+                                await deleteUser(user)//Apaga do firebase Auth
+                                await AsyncStorage.removeItem('@user')
+                                Alert.alert("Conta Excluída", "Sua conta foi excluída com sucesso!")
+                                router.replace('/')
+                            } else{
+                                Alert.alert("Erro", "Nenhum usuário logado.")
+                            }
+                        } catch (error) {
+                            console.log("Erro ao excluir a conta: ", error);
+                            Alert.alert("Error", "Não foi possível excluir conta")
+                        }
+                    }
+                }
+            ]
+        )
+    }
+
+    const salvarItem = async() => {
+        try{
+            const docRef = await addDoc(collection(db,'items'),{
+                title: title,
+                isChecked: false, // botão que vai atualizar automaticamente
+            })
+            Alert.alert("Sucesso", "Produto salvo com sucesso!")
+            console.log("Documento Salvo", docRef.id);
+            setTitle('')//Limpa o TextInput
+        }catch(e){
+            console.error("Error adding document: ", e);
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Seja bem-vindo a Tela Inicial da Aplicação</Text>
+            <Button title="Sair da Conta" onPress={realizarLogoff}/>
+            <Button title="Exluir conta" color='red' onPress={excluirConta}/>
+            <Button title="Alterar Senha" onPress={() => router.push("/AlterarSenha")}/>
+            <ItemLoja />
+            <ItemLoja />
+            <ItemLoja />
+            <TextInput  placeholder="Digite o nome do produto" 
+                        style={styles.input}
+                        value={title}
+                        onChangeText={(value) => setTitle(value)}
+                        onSubmitEditing={salvarItem}/>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    input:{
+        backgroundColor: 'lightgray',
+        padding: 10,
+        fontSize: 15,
+        width: '90%',
+        alignSelf: 'center',
+        borderRadius: 10,
+        marginTop: 20,
+    }
+})
